b04d85bb92d02abcc02d39a512214456be3b9b64
Paul C. Owe <40435982+owecodes@users.noreply.github.com>
2021-07-28 12:49:56 -0400

    fix credential issues

--- create-service-account.sh
+++ create-service-account.sh
@@ -1,2 +1,7 @@
 gcloud iam service-accounts create terraform
+
 gcloud iam service-accounts keys create ./credentials.json --iam-account terraform@qwiklabs-gcp-04-b75f7be70aab.iam.gserviceaccount.com
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:terraform@qwiklabs-gcp-04-b75f7be70aab.iam.gserviceaccount.com --role=roles/owner
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID-state-bucket
